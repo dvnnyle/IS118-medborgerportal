f98b675a4278b925bb9444be4e9a6870db554aee
dvnny <godannyyy@gmail.com>
2025-11-18 15:30:39 +0100

    Updated master.py with improved start interface and clean formatting

--- innleveringsoppgave2/master.py
+++ innleveringsoppgave2/master.py
@@ -1,13 +1,23 @@
-print("danny var her")
-
-
-
-print("\033[1m\033[91m\nErlings Prosjekt \n\033[0m")
-print("👾 - Du spiller som Erling, prosjektleder for kommunens nye medborgerportal.")
-print("👾 - Teamet begynner å møte utfordringer. Du må ta fem viktige valg for å lede dem videre.\n")
-
 
 #styling ANSI
 BOLD = "\033[1m"
 RED = "\033[91m" 
 RESET = "\033[0m"
+
+print(f"{BOLD}{RED}\nErlings Prosjekt \n{RESET}")
+print("Du spiller som Erling, prosjektleder for kommunens nye medborgerportal.")
+print("Teamet begynner å møte utfordringer. Du må ta tre viktige valg for å lede dem videre.\n")
+
+print("Tast A eller B for å starte:")
+print("A = JA (Start simulasjonen)")
+print("B = NEI (Avslutt)")
+start_valg = input("\nDitt valg: ").upper()
+
+if start_valg in ['A', 'JA', 'J']:
+    print("Flott! La oss begynne!\n")
+elif start_valg in ['B', 'NEI', 'N']:
+    print("Ingen problem! Ta deg tid når du er klar.")
+    exit()
+else:
+    print("LOL, Du har ikke noe valg. Starter simulasjonen likevel...\n")
+
